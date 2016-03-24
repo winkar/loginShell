bf7f39c68d15ad902b19db84692295b565c6d602
陈钧衍 <winkar@163.com>
2016-03-24 16:19:27 +0800

    将脚本转移到scripts文件夹中

--- Automator/src/main/scala/com/winkar/AndroidUtils.scala
+++ Automator/src/main/scala/com/winkar/AndroidUtils.scala
@@ -1,26 +1,10 @@
 package com.winkar
 
-import scala.sys.process._;
+import scala.sys.process._
 
 
 object AndroidUtils {
-  private def checkOutput(cmd: String): String = cmd.!!
+  def getPackageName(apkPath: String): String = s"scripts/getPackageName.sh ${apkPath}".!!.trim
 
-
-  def getPackageName(apkPath: String): String = apkPath.split("/").last.split("\\.").dropRight(1).mkString(".")
-
-
-  def getMainActivity(apkPath: String): String = checkOutput(s"aapt dump badging $apkPath " +
-    " | awk -F\" \" '/launchable-activity/ {print $2}'  " +
-    " |awk -F\"'\" \'/name=/ {print $2}'")
-
-  def getCurrentPackage() = "/usr/local/bin/adb shell dumpsys window windows "
-    .!!
-    .split("\n")
-    .filter(_.contains("mCurrentFocus"))
-    .take(1)(0)
-    .split("/")(0)
-    .reverse.split(" ")(0)
-    .reverse
-
+  def getCurrentPackage() = "scripts/getCurrentPackage.sh".!!.trim
 }
