13b2c41fbc69f48aec3c083db81c42665698f88c
陈钧衍 <winkar@163.com>
2016-03-30 16:10:40 +0800

    增加了traversal timeout功能

--- Automator/src/main/scala/com/winkar/AppTester.scala
+++ Automator/src/main/scala/com/winkar/AppTester.scala
@@ -15,15 +15,19 @@ trait AppTester {
 class MultiAppTester(var apkDirectoryRoot: String) extends AppTester {
   val appBlackList = Array("aimoxiu.theme.mx49c81e403f35f52d4cdc6ad2020da3d8.apk",
                             "aimoxiu.theme.mx62fbed7a2d8bc5f11a4a35ae0289a3b3.apk")
-  @throws[IOException]
+
   override def startTest {
     val apkRoot: File = new File(apkDirectoryRoot)
     for (path <- apkRoot.list) {
-      if (!appBlackList.contains(path)) {
-        log.info(String.format("Testing apk %s", path))
-        val appTraversal: AppTraversal = new AppTraversal(apkDirectoryRoot + File.separator + path)
-        appTraversal.start
-        log.info(String.format("Stop testing apk %s", path))
+      try {
+        if (!appBlackList.contains(path)) {
+          log.info(String.format("Testing apk %s", path))
+          val appTraversal: AppTraversal = new AppTraversal(apkDirectoryRoot + File.separator + path)
+          appTraversal.start
+          log.info(String.format("Stop testing apk %s", path))
+        }
+      } catch {
+        case e: org.openqa.selenium.WebDriverException => e.printStackTrace
       }
     }
   }
@@ -32,6 +36,10 @@ class MultiAppTester(var apkDirectoryRoot: String) extends AppTester {
 
 class SingleAppTester private[winkar](val apkPath: String) extends AppTester {
   override def startTest: Unit = {
-    new AppTraversal(apkPath).start
+    try {
+      new AppTraversal(apkPath).start
+    } catch  {
+      case e: org.openqa.selenium.WebDriverException => e.printStackTrace
+    }
   }
 }
